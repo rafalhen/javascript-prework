54adfe6e4a55850acfa3afd8f485eacc90f74c50
Rafal Hen <rafalhen@poczta.onet.pl>
2020-11-11 17:40:59 +0100

    logical1

--- js/script.js
+++ js/script.js
@@ -16,7 +16,6 @@ function getMoveName(argMoveId){
     }
 }
 
-console.log
 /* computer */
 
 let randomNumber = Math.floor(Math.random() * 3 + 1);
@@ -34,27 +33,21 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = getMoveName(randomNumber);
+let playerMove = getMoveName(playerInput);
 
 printMessage('Twój ruch to: ' + playerMove);
+
 
 /* results */
 
 function displayResult(argComputerMove, argPlayerMove){
     printMessage('Zagrałem' + argComputerMove + ', a ty ' + argPlayerMove);
-}
 if( argComputerMove == 'kamień' && argPlayerMove == 'papier') {
     printMessage('Ty Wygrywasz');
 } else if( argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
     printMessage('Ty Wygrywasz');
 } else if( argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
     printMessage('Ty Wygrywasz');
-} else if( argComputerMove == 'kamień' && argPlayerMove == 'kamień') {
-    printMessage('Remis');
-} else if( argComputerMove == 'papier' && argPlayerMove == 'papier') {
-    printMessage('Remis');
-} else if( argComputerMove == 'nożyce' && argPlayerMove == 'nożyce') {
-    printMessage('Remis');
 } else if( argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
     printMessage('Przegrywasz');
 } else if( argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
@@ -62,8 +55,9 @@ if( argComputerMove == 'kamień' && argPlayerMove == 'papier') {
 } else if( argComputerMove == 'papier' && argPlayerMove == 'kamień') {
     printMessage('Przegrywasz');
 } else if(argComputerMove == argPlayerMove) {
-    printMessage(Remis);
+    printMessage('Remis');
 } else {
     printMessage('Gra nierostrzygnięta');
 }
 
+}
