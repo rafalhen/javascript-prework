292ee1c801849df75e9a50cef01c90362c29527a
Rafal Hen <rafalhen@poczta.onet.pl>
2020-11-13 16:29:20 +0100

    game

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
 
 /* Function */
 
+function playGame(playerImput){
+    clearMessages();
+
 function getMoveName(argMoveId){
     if(argMoveId == 1){
       return 'kamień';
@@ -28,15 +31,14 @@ let computerMove = getMoveName(randomNumber);
 printMessage('Mój ruch to: ' + computerMove);
 
 /* player */
-
+/*
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
+*/
 console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = getMoveName(playerInput);
 
 printMessage('Twój ruch to: ' + playerMove);
-
 
 /* results */
 
@@ -58,6 +60,19 @@ if( argComputerMove == 'kamień' && argPlayerMove == 'papier') {
     printMessage('Remis');
 } else {
     printMessage('Gra nierostrzygnięta');
-}
+}}
 
+displayResult(computerMove, playerMove);
 }
+document.getElementById('play-rock').addEventListener('click', function(){
+    playGame('1');
+  });
+
+document.getElementById('play-paper').addEventListener('click', function(){
+    playGame('2');
+  });
+
+document.getElementById('play-scissors').addEventListener('click', function(){
+    playGame('3');
+  });
+
